6d3961338785d78bc64b098cc8b37e70cdf8f212
Moe-Ahmadi <amohamadreza2113@gmail.com>
2021-02-05 20:24:09 +0000

    first commit

---
+++ Lesson1.cpp
@@ -0,0 +1,26 @@
+/*
+ * Lesson1.cpp
+ *
+ *  Created on: Feb 5, 2021
+ *      Author: amohamadreza2113
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age;
+	int idNumber;
+	cout <<"Please enter your age: ";
+	cin >>age;
+	cout <<"Please enter you id number: ";
+	cin >>idNumber;
+
+
+	cout << "the age is: " <<age <<"\nthe id number is:" <<idNumber;
+
+	return 0;
+
+}
+
+
+
